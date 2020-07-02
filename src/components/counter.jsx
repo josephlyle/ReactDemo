e6113b640d718d52fb7d3c17a4520612bdbf908b
joey <joefuller000@gmail.com>
2020-07-01 21:52:58 -0500

    demo finished, not polished

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,30 +1,50 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200",
-  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // Ajax call and get new data from the server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
 
   render() {
     return (
-      <React.Fragment>
-        {/* generates a random 200px img */}
-        <img src={this.state.imageUrl} alt="" /> {/* now do the buttons */}
+      <div>
         <span className={this.getAlertClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter, true)}
+          className="btn ml-3 mr-1 btn-secondary btn-sm"
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter, false)}
+          className="btn mr-3 btn-secondary btn-sm"
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Remove
+        </button>
+      </div>
     );
   }
 
   getAlertClasses() {
-    let classes = "alert m-2 alert-";
-    classes += this.state.count === 0 ? "primary" : "success";
+    let classes = "alert ml-5 pb-2 pt-2 alert-";
+    classes += this.props.counter.value === 0 ? "primary" : "success";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
